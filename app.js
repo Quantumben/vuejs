4941e615e102a5d048d7a39617c031f4b654f8fe
QuantumBen <omosabenedict@gmail.com>
2022-10-21 14:36:57 +0100

    v-bind directives

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const vm = Vue.createApp({
     data(){
         return {
             firstName: 'John',
-            lastName: 'Doe'
+            lastName: 'Doe',
+            url: 'http://google.com',
         }
     },
     methods: { 
