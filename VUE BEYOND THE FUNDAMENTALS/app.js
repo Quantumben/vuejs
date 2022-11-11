46208d25fdd8d74ba12bef4f863f295220686ef7
QuantumBen <omosabenedict@gmail.com>
2022-11-11 03:02:19 +0100

    Vue Component introduction

--- VUE BEYOND THE FUNDAMENTALS/app.js
+++ VUE BEYOND THE FUNDAMENTALS/app.js
@@ -1,26 +1,19 @@
 let vm = Vue.createApp({
-  data() {
-    return {
-      message: "Hello world!"
-    }
-  },
+  // data() {
+  //   return {
+  //     message: "Hello world!"
+  //   }
+  // },
   //template: ``
 })
 
+vm.component('hello', {
+  template: `<h1>{{ message }}</h1>`,
+  data() {
+    return {
+      message: 'Hello World Ben!'
+    }
+  }
+})
+
 vm.mount('#app')
-
-// vm2 = Vue.createApp({
-//   data(){
-//     return {
-//       message: "Hello world"
-//     }
-//   },
-//   render(){
-//     return Vue.h(
-//       'h1',
-//       this.message
-//     )
-//   }
-// }).mount("#app2")
-
-//h => hyperscript
